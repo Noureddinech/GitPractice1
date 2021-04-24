b66323da84f19a7019f925da232b023a68a073a0
Noureddinech <Noureddineleroi@gmail.com>
2021-04-24 16:52:47 -0400

    adding more work

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("adding Github now");
+        System.out.println("new commit added to check");
     }
 }
