7acfa3f04092e1bb9b7728a15a62d5c4d3c31ff1
Noureddinech <Noureddineleroi@gmail.com>
2021-04-24 16:57:00 -0400

    wrap up code

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
         System.out.println("Hello world");
         System.out.println("adding Github now");
         System.out.println("new commit added to check");
+        System.out.println("adding more codes");
+        // adding final wrap up code
     }
 }
